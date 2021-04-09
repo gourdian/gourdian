90c35bf6b0fcc684d5686e718330b9fff5b20241
Rob Crowell <rob@gourdian.net>
2021-04-09 03:02:31 +0000

    Add requirements to setup.py.

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ from distutils.core import setup
 setup(
   name='gourdian',
   version='0.1',
+  install_requires=[
+    # Core deps.
+    'more_itertools', 'pandas', 'requests',
+    # Terminal-based drawing deps for gourd cli.
+    'asyncio', 'colored', 'urwid',
+  ],
 )
